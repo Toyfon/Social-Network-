2ef2754c46653aec3e417bcd292d54ac9c347c67
Toyfon <90236783+Toyfon@users.noreply.github.com>
2021-12-21 21:50:48 +0300

    (75) formik. Add login page

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,7 +1,50 @@
+import {Field, Form, Formik, FormikHelpers} from "formik";
+
+export const Login = () => {
+    console.log()
+    return (<div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
+    )
+}
 
 
-export const Login =() => {
-    return ( <div>
-        <h1>login</h1>
-    </div>
-    )}
+type Values = {
+    login:string
+    password:string
+}
+
+const LoginForm = () => {
+    const initialValues: Values = {login:'', password:''}
+    return (<div>
+            <Formik
+            initialValues={initialValues}
+            onSubmit={(
+                values: Values,
+                { setSubmitting }: FormikHelpers<Values>
+            ) => {
+                setTimeout(() => {
+                    console.log(values)
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 500);
+            }}>
+            <Form>
+                <div>
+                    <Field id={'login'} name={'login'} placeholder={"Login"}/>
+                </div>
+                <div>
+                    <Field id={'password'} name={'password'} placeholder={"Password"}/>
+                </div>
+                <div>
+                    <Field id={'rememberMe'} name={'rememberMe'} type={"checkbox"}/> remember me
+                </div>
+                <div>
+                    <button type={'submit'}>Login</button>
+                </div>
+            </Form>
+            </Formik>
+        </div>
+    )
+}
